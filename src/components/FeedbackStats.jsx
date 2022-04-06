a91b9bf90a88f53db8d0c83dffd4d48cdffdcd02
Filipe Mendes <5122694+filipe2m@users.noreply.github.com>
2022-04-06 02:46:34 +0100

    feature: added update option

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,9 @@
-import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
   //Calculate ratings average
   let average =
     feedback.reduce((acc, cur) => {
@@ -16,15 +19,5 @@ function FeedbackStats({ feedback }) {
     </div>
   );
 }
-
-FeedbackStats.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired
-    })
-  )
-};
 
 export default FeedbackStats;
